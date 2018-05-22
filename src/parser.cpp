c67426013d61c7957566ccfc6d2bc37148320127
P. Gilbert Schmitt <pgilbertschmitt@gmail.com>
2018-05-21 19:05:15 -0700

    Add precedences to map

--- src/parser.cpp
+++ src/parser.cpp
@@ -2,6 +2,25 @@
 
 Parser::Parser(Lexer* lexer) : lexer(lexer) {
   // Set precedences
+  precedences[TokenType::ASSIGN] = rank::ASSIGN;
+
+  precedences[TokenType::EQ] = rank::EQUALS;
+  precedences[TokenType::NEQ] = rank::EQUALS;
+
+  precedences[TokenType::LT] = rank::COMPARE;
+  precedences[TokenType::GT] = rank::COMPARE;
+  precedences[TokenType::LTEQ] = rank::COMPARE;
+  precedences[TokenType::GTEQ] = rank::COMPARE;
+
+  precedences[TokenType::PLUS] = rank::SUM;
+  precedences[TokenType::MINUS] = rank::SUM;
+
+  precedences[TokenType::ASTERISK] = rank::PRODUCT;
+  precedences[TokenType::SLASH] = rank::PRODUCT;
+  precedences[TokenType::MODULO] = rank::PRODUCT;
+
+  precedences[TokenType::LPAREN] = rank::CALL;
+  precedences[TokenType::LBRACKET] = rank::INDEX;
 
   // Register Prefix Parsing functions here
 
