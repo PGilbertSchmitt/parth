32450d53e6331c901b479e838a27f084d6043a9f
P. Gilbert Schmitt <pgilbertschmitt@gmail.com>
2018-05-22 19:37:04 -0700

    Add data accessors and checkers

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,6 +1,7 @@
 #include "parser.h"
 
-std::unordered_map<TokenType, int, EnumClassHash> Parser::precedences{
+// Defining static rank mappings
+rank_map Parser::precedences{
     {TokenType::ASSIGN, rank::ASSIGN}, {TokenType::EQ, rank::EQUALS},
     {TokenType::NEQ, rank::EQUALS},    {TokenType::LT, rank::COMPARE},
     {TokenType::GT, rank::COMPARE},    {TokenType::LTEQ, rank::COMPARE},
@@ -23,6 +24,49 @@ void Parser::register_prefix(TokenType tt, PPF ppf) {
 void Parser::register_infix(TokenType tt, IPF ipf) {
   infix_parsers.emplace(int(tt), ipf);
 }
+
+/* Accessors and checkers for parsing functions */
+
+Token Parser::get_cur_token() { return cur_token; }
+
+Token Parser::get_peek_token() { return peek_token; }
+
+bool Parser::cur_token_is(TokenType tt) { return tt == cur_token.getType(); }
+
+bool Parser::peek_token_is(TokenType tt) { return tt == peek_token.getType(); }
+
+bool Parser::expect_peek(TokenType tt) {
+  if (peek_token_is(tt)) {
+    next_token();
+    return true;
+  }
+
+  // TODO: Add peek error
+  return false;
+}
+
+void Parser::next_token() {
+  cur_token = peek_token;
+  peek_token = lexer->next_token();
+}
+
+Parser::rank Parser::cur_precedence() {
+  rank_map::iterator cur_rank = Parser::precedences.find(cur_token.getType());
+  if (cur_rank == Parser::precedences.end()) {
+    return rank::LOWEST;
+  }
+  return rank(cur_rank->second);
+}
+
+Parser::rank Parser::peek_precedence() {
+  rank_map::iterator peek_rank = Parser::precedences.find(peek_token.getType());
+  if (peek_rank == Parser::precedences.end()) {
+    return rank::LOWEST;
+  }
+  return rank(peek_rank->second);
+}
+
+/* Prefix Parsers */
 
 ast::node_ptr parse_identifier(Parser& p) {
   Token cur = p.get_cur_token();
