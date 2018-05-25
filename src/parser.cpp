82604b4c6baccd65bd16c2a6f8c7924d2d2cc9de
P. Gilbert Schmitt <pgilbertschmitt@gmail.com>
2018-05-24 20:35:11 -0700

    Begin parsing lines; add printouts for bug tracking

--- src/parser.cpp
+++ src/parser.cpp
@@ -13,8 +13,14 @@ rank_map Parser::precedences{
 Parser::Parser(Lexer* lexer) : lexer(lexer) {
   // Register Prefix Parsing functions here
   register_prefix(TokenType::IDENT, &parse_identifier);
+  register_prefix(TokenType::INT, &parse_identifier);
 
   // Register Infix Parsing functions here
+
+  this->next_token();
+  this->next_token();
+  std::cout << "CUR PARSED TOKEN IS " << token_type_string(cur_token.get_type())
+            << std::endl;
 }
 
 void Parser::register_prefix(TokenType tt, PPF ppf) {
@@ -71,9 +77,14 @@ ast::block_ptr Parser::parse_program() {
   Token block_token = Token(TokenType::NONE, "block", 0, 0);
   ast::block_ptr block = ast::block_ptr(new ast::Block(block_token));
 
+  int i = 0;
   while (cur_token.get_type() != TokenType::EOF_VAL) {
+    std::cout << "BEGIN parsing Line " << i << std::endl;
     ast::node_ptr new_node = this->parse_line();
     block->push_node(new_node);
+    this->next_token();
+    std::cout << "DONE  psarsing Line " << i << std::endl;
+    i++;
   }
 
   return block;
@@ -82,25 +93,40 @@ ast::block_ptr Parser::parse_program() {
 ast::node_ptr Parser::parse_line() {
   ast::node_ptr line = this->parse_expression(rank::LOWEST);
 
-  while (this->cur_token.get_type() == TokenType::NEWLINE) {
-    this->lexer->next_token();
+  if (!this->peek_token_is(TokenType::NEWLINE)) {
+    // TODO: Raise error of some kind. Line expressions (any expression not
+    // contained within another expression) must be ended with newlines. Maybe
+    // consider semicolons as an option.
   }
 
+  while (this->cur_token_is(TokenType::NEWLINE)) {
+    this->lexer->next_token();
+  }
   return line;
 }
 
 ast::node_ptr Parser::parse_expression(rank r) {
   int cur_type = int(this->cur_token.get_type());
-  PPF prefix = this->prefix_parsers[cur_type];
-  if (prefix == NULL) {
-    // Add prefix parsing error
+  prefix_map::iterator prefix = this->prefix_parsers.find(cur_type);
+  if (prefix == this->prefix_parsers.end()) {
+    // TODO: Add prefix parsing error
+    std::cout << "NO SUCH PREFIX PARSER FOR "
+              << token_type_string(cur_token.get_type()) << std::endl;
+    std::cout << "Current token lit is " << cur_token.get_literal()
+              << std::endl;
     return ast::node_ptr();
   }
-  ast::node_ptr left_expr = prefix(*this);
+  ast::node_ptr left_expr = prefix->second(*this);
 
   while (!this->peek_token_is(TokenType::NEWLINE) &&
          (r < this->peek_precedence())) {
-    // Do stuff
+    infix_map::iterator infix = this->infix_parsers.find(cur_type);
+    if (infix == this->infix_parsers.end()) {
+      return left_expr;
+    }
+
+    this->next_token();
+    left_expr = infix->second(*this, left_expr);
   }
 
   return left_expr;
