1ae9aafca638db849c72a62f8e38ddb04faf9d7e
P. Gilbert Schmitt <pgilbertschmitt@gmail.com>
2018-06-07 07:53:17 -0700

    Add base level environment tests

--- test/environment_test.cpp
+++ test/environment_test.cpp
@@ -8,13 +8,43 @@ using namespace env;
 
 TEST(Environment, Init) {
   env_ptr e = env_ptr(new Environment());
+  std::string key = "key";
   int value = 25;
   obj::obj_ptr int_obj = obj::int_ptr(new obj::Integer(value));
 
-  ASSERT_NO_THROW(e->init("key", int_obj));
-  ASSERT_ANY_THROW(e->init("key", int_obj))
+  ASSERT_NO_THROW(e->init(key, int_obj));
+  ASSERT_ANY_THROW(e->init(key, int_obj))
       << "Init should not initialize the same variable in the same environment "
          "level";
+}
+
+TEST(Environment, Get) {
+  env_ptr e = env_ptr(new Environment());
+  std::string key = "key";
+  int value = 25;
+  obj::obj_ptr int_obj = obj::int_ptr(new obj::Integer(value));
+  e->init(key, int_obj);
+
+  ASSERT_NO_THROW(e->get(key));
+  ASSERT_TRUE(e->get(key) == int_obj) << "::get() should return correct object";
+  ASSERT_NO_THROW(std::dynamic_pointer_cast<obj::Integer>(e->get(key)))
+      << "Object should be correct type";
+}
+
+TEST(Environment, Set) {
+  env_ptr e = env_ptr(new Environment());
+  std::string key = "key";
+  int value = 25;
+  obj::obj_ptr int_obj = obj::int_ptr(new obj::Integer(value));
+  int new_value = 50;
+  obj::obj_ptr new_int_obj = obj::int_ptr(new obj::Integer(new_value));
+  e->init(key, int_obj);
+
+  ASSERT_NO_THROW(e->set(key, new_int_obj));
+  ASSERT_TRUE(e->get(key) == new_int_obj)
+      << "::set() should cause ::Get() to return correct object";
+  ASSERT_NO_THROW(std::dynamic_pointer_cast<obj::Integer>(e->get(key)))
+      << "Object should be correct type";
 }
 
 TEST(Environment, StackedInit) {
