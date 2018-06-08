a5c4ab9cb062c459f4b5a7c193a01f349f4e3341
P. Gilbert Schmitt <pgilbertschmitt@gmail.com>
2018-06-08 07:51:11 -0700

    Recursively check outer scopes

--- src/environment.cpp
+++ src/environment.cpp
@@ -16,7 +16,10 @@ void Environment::init(const std::string &key, obj::obj_ptr value) {
 void Environment::set(const std::string &key, obj::obj_ptr value) {
   if (this->store.find(key) != this->store.end()) {
     this->store[key] = value;
+  } else if (this->outer != nullptr) {
+    this->outer->set(key, value);
   } else {
+    std::cout << "Outer exists: " << (this->outer == nullptr) << std::endl;
     throw NoVarException(key);
   }
 }
@@ -24,6 +27,8 @@ void Environment::set(const std::string &key, obj::obj_ptr value) {
 obj::obj_ptr Environment::get(const std::string key) {
   if (this->store.find(key) != this->store.end()) {
     return this->store[key];
+  } else if (this->outer != nullptr) {
+    return this->outer->get(key);
   } else {
     throw NoVarException(key);
   }
