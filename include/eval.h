4202faa51d77a43c748f0a7b8aa1cb33db8078ed
P. Gilbert Schmitt <pgilbertschmitt@gmail.com>
2018-06-10 13:17:18 -0700

    Evaluate infix expressions

--- include/eval.h
+++ include/eval.h
@@ -6,6 +6,7 @@
 #include "ast.h"
 #include "environment.h"
 #include "object.h"
+#include "parth_error.h"
 
 const obj::bool_ptr TRUE_OBJ = obj::bool_ptr(new obj::Boolean(true));
 const obj::bool_ptr FALSE_OBJ = obj::bool_ptr(new obj::Boolean(false));
@@ -15,6 +16,10 @@ obj::obj_ptr eval(ast::node_ptr, env::env_ptr);
 obj::obj_ptr evalBlock(ast::block_ptr, env::env_ptr);
 obj::obj_ptr evalIdent(ast::ident_ptr, env::env_ptr);
 obj::obj_ptr evalLet(ast::let_ptr, env::env_ptr);
+obj::int_ptr evalInteger(ast::int_ptr);
 obj::bool_ptr evalBoolean(ast::bool_ptr);
+obj::obj_ptr evalInfix(ast::infix_ptr, env::env_ptr);
+obj::obj_ptr evalAssign(ast::ident_ptr, ast::node_ptr, env::env_ptr);
+obj::obj_ptr evalIntegerInfixOperator(Token, ast::int_ptr, ast::int_ptr);
 
 #endif
--- src/eval.cpp
+++ src/eval.cpp
@@ -18,14 +18,12 @@ obj::obj_ptr eval(ast::node_ptr node, env::env_ptr envir) {
       return evalLet(let_node, envir);
     } break;
 
-    case ast::ASSIGN: {
-      ast::assign_ptr assign_node =
-          std::dynamic_pointer_cast<ast::Assign>(node);
-      return evalAssign(assign_node, envir);
-    } break;
-
       // case ast::RETURN:
-      // case ast::INTEGER:
+
+    case ast::INTEGER: {
+      ast::int_ptr int_node = std::dynamic_pointer_cast<ast::Integer>(node);
+      return evalInteger(int_node);
+    } break;
 
     case ast::BOOLEAN: {
       ast::bool_ptr bool_node = std::dynamic_pointer_cast<ast::Boolean>(node);
@@ -34,7 +32,12 @@ obj::obj_ptr eval(ast::node_ptr node, env::env_ptr envir) {
 
       // case ast::OPTION:
       // case ast::PREFIX:
-      // case ast::INFIX:
+
+    case ast::INFIX: {
+      ast::infix_ptr infix_node = std::dynamic_pointer_cast<ast::Infix>(node);
+      return evalInfix(infix_node, envir);
+    } break;
+
       // case ast::IF_ELSE:
 
     default: {
@@ -72,6 +75,7 @@ obj::obj_ptr evalIdent(ast::ident_ptr ident, env::env_ptr envir) {
   } else {
     std::cout << "No such identifier " << ident->value
               << ". Must be a builtin?\n";
+    return value;
   }
 }
 
@@ -83,12 +87,8 @@ obj::obj_ptr evalLet(ast::let_ptr let, env::env_ptr envir) {
   return right;
 }
 
-obj::obj_ptr evalAssign(ast::assign_ptr assign, env::env_ptr envir) {
-  obj::obj_ptr right = eval(assign->expression, envir);
-  if (right->_type() != obj::ERROR) {
-    envir->set(assign->name->value, right);
-  }
-  return right;
+obj::int_ptr evalInteger(ast::int_ptr int_node) {
+  return obj::int_ptr(new obj::Integer(int_node->value));
 }
 
 obj::bool_ptr evalBoolean(ast::bool_ptr bool_node) {
@@ -97,4 +97,88 @@ obj::bool_ptr evalBoolean(ast::bool_ptr bool_node) {
   } else {
     return FALSE_OBJ;
   };
+}
+
+obj::obj_ptr evalInfix(ast::infix_ptr infix_node, env::env_ptr envir) {
+  // Special cases first
+  Token op = infix_node->op;
+  ast::node_ptr left_node = infix_node->left;
+  ast::node_ptr right_node = infix_node->right;
+
+  if (left_node->_type() == ast::IDENT && op.get_type() == TokenType::ASSIGN) {
+    std::cout << "WE HAVE ASSIGNMENT!\n";
+    ast::ident_ptr left = std::dynamic_pointer_cast<ast::Identifier>(left_node);
+    return evalAssign(left, right_node, envir);
+  }
+
+  if (left_node->_type() == ast::INTEGER &&
+      right_node->_type() == ast::INTEGER) {
+    std::cout << "WE HAVE OPERATION!\n";
+    ast::int_ptr left = std::dynamic_pointer_cast<ast::Integer>(left_node);
+    ast::int_ptr right = std::dynamic_pointer_cast<ast::Integer>(right_node);
+    return evalIntegerInfixOperator(op, left, right);
+  }
+
+  std::string message =
+      "No such operation " + ast::node_type_string(left_node->_type()) + " " +
+      op.get_literal() + " " + ast::node_type_string(right_node->_type());
+  throw NoSuchOperatorException(message);
+}
+
+obj::obj_ptr evalAssign(ast::ident_ptr left, ast::node_ptr right,
+                        env::env_ptr envir) {
+  obj::obj_ptr value = eval(right, envir);
+  if (value->_type() != obj::ERROR) {
+    envir->set(left->value, value);
+  }
+  return value;
+}
+
+obj::obj_ptr evalIntegerInfixOperator(Token op, ast::int_ptr left,
+                                      ast::int_ptr right) {
+  switch (op.get_type()) {
+    case TokenType::PLUS: {
+      return obj::int_ptr(new obj::Integer(left->value + right->value));
+    }
+    case TokenType::MINUS: {
+      return obj::int_ptr(new obj::Integer(left->value - right->value));
+    }
+    case TokenType::ASTERISK: {
+      return obj::int_ptr(new obj::Integer(left->value * right->value));
+    }
+    case TokenType::SLASH: {
+      if (right->value == 0) {
+        throw DivideByZeroException(op);
+      }
+      return obj::int_ptr(new obj::Integer(left->value / right->value));
+    }
+    case TokenType::MODULO: {
+      if (right->value == 0) {
+        throw DivideByZeroException(op);
+      }
+      return obj::int_ptr(new obj::Integer(left->value % right->value));
+    }
+    case TokenType::EQ: {
+      return obj::bool_ptr(new obj::Boolean(left->value == right->value));
+    }
+    case TokenType::NEQ: {
+      return obj::bool_ptr(new obj::Boolean(left->value != right->value));
+    }
+    case TokenType::LT: {
+      return obj::bool_ptr(new obj::Boolean(left->value < right->value));
+    }
+    case TokenType::GT: {
+      return obj::bool_ptr(new obj::Boolean(left->value > right->value));
+    }
+    case TokenType::LTEQ: {
+      return obj::bool_ptr(new obj::Boolean(left->value <= right->value));
+    }
+    case TokenType::GTEQ: {
+      return obj::bool_ptr(new obj::Boolean(left->value >= right->value));
+    }
+    default: {
+      throw NoSuchOperatorException("No such operation INT " +
+                                    op.get_literal() + " INT\n");
+    }
+  }
 }
