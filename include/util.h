a3c927ae0872fc3c94f0c6ba2e7f97e6c02e090a
P. Gilbert Schmitt <pgilbertschmitt@gmail.com>
2018-11-18 14:19:48 -0800

    Add simple debugging utility

---
+++ include/util.h
@@ -0,0 +1,9 @@
+#ifndef PARTH_UTIL_H
+#define PARTH_UTIL_H
+
+#include <iostream>
+
+// For quick debugging-via-prints;
+void bust(std::string str);
+
+#endif
---
+++ src/util.cpp
@@ -0,0 +1,3 @@
+#include "util.h"
+
+void bust(std::string str) { std::cout << str << std::endl; }
